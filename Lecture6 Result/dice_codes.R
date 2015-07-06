10be791b7a41596f09565fa64d13ed5195c3537c
khscor <khscor@gmail.com>
2015-07-06 10:09:10 +0900

    updates

--- Lecture6 Result/dice_codes.R
+++ Lecture6 Result/dice_codes.R
@@ -1,13 +1,22 @@
-
+#function constructing
 dicefunction=function(r){
+  #set initial value, but it'll be not concluded in result
   C=0
+    #for loop to repeat
     for (i in 1:r+1){
+      #I used sample function
     A=sample(1:6,1)
     B=sample(1:6,1)
     C=c(C,A+B)}
+    #discare the initial value
     C[-1]
 }
-
+#plottings
 plot(dicefunction(10))
 plot(dicefunction(100))
 plot(dicefunction(10000))
+
+#Drawing thistograms
+hist(dicefunction(10))
+hist(dicefunction(100))
+hist(dicefunction(10000))
