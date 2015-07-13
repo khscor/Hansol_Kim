35830afd0bb43ef7db7d631cc048c959db31df4b
Hansol_Kim <khscor@gmail.com>
2015-07-13 11:48:06 +0900

    remend

--- Lecture 11 Result/code_11.R
+++ Lecture 11 Result/code_11.R
@@ -9,6 +9,6 @@ p=qplot(studytime,G3,data=a)
 p+geom_abline(colour="blue")
 coef(lm(G3~studytime,data=a))
 
-#plot the distributions of scores (fisrt, second, and third test)
+#plot the distribution of score
 k=qplot(G3,data=a,geom="density")
 
